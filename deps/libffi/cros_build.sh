2664fcf9d09380fb107af6ae42e6a1a474a3ac18
NzSN <nzsn0101@gmail.com>
2024-09-02 15:47:01 +0800

    Update cros_build.sh

--- deps/libffi/cros_build.sh
+++ deps/libffi/cros_build.sh
@@ -1,9 +1,9 @@
 ./configure \
-  --with-sysroot=/home/nzsn/Misc/LoongArch/Tools/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.5/loongarch64-linux-gnu/sysroot \
+  --with-sysroot=path/to/sysroot \
   --target=loongarch64-unknown-linux-gnu \
   --host=loongarch64-unknown-linux-gnu \
   --build=x86_64-unknown-linux-gnu  \
-  --prefix=/home/nzsn/Codebases/libffi/dist \
+  --prefix=path/of/prefix \
   --enable-static --disable-shared --disable-builddir --with-pic \
-  CC=/home/nzsn/Misc/LoongArch/Tools/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.5/bin/loongarch64-linux-gnu-gcc \
+  CC=cc \
   CFLAGS="-O3"
